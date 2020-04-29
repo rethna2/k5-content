0e6b73dce3b94c2e503a309a93542088f8962eb6
Rethna Ganesh <rethna2@gmail.com>
2020-04-29 10:11:30 +0530

    Update and rename correct spelling.md to correctSpelling.js

---
+++ english/vocabulary/correctSpelling.js
@@ -0,0 +1,47 @@
+// Pick the word that has the correct spelling
+
+/*
+Notes: There are 5 levels of complexity. So add the words using your judgement. The first entry should be with correct spelling, and the remaining words should be misspelled. The number of entries should be between 2 and 4.
+*/
+
+const level_1 = `flies flys
+class clas 
+cold kold
+memory memary 
+butter buter
+sugar suger
+water watar
+once onse
+cattle catle
+chilli chily
+boss bos
+sister sistr
+better beter
+wheat wheet
+berries berrys
+fries frys
+meals meels
+doctor doktor
+bomb bom
+ghost gost
+potatoes potatos
+listen lisen 
+seed ceed
+tomatoes tomatos
+catch katch
+raspberry rasberry`
+
+const level_2 = `absence abcense
+queue que
+column colum
+extreme extreem
+almost allmost
+beginning begining
+surprise suprise
+calendar calender
+vehicle vehical
+difference diffrence
+awesome ausom
+giraffe girafee
+meaning meening
+address adress`
