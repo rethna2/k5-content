ecca17f4402abc00b93a1cc79ea1c4c72af4866a
Rethna Ganesh <rethna2@gmail.com>
2020-04-29 12:11:13 +0530

    Create fillup.js

---
+++ english/grammar/fillup.js
@@ -0,0 +1,68 @@
+const wasWere = `It ~was~ very hot yesterday.
+~Were~ you angry yesterday?
+I ~was~ having fun at the park.
+Where ~were~ you?
+Why ~were~ you crying?
+They ~were~ drinking water.
+I ~was~ winning the race!
+Where ~were~ you going?
+I ~was~ so happy yesterday!
+When ~were~ you at the park?
+They ~were~ going to zoo.
+Why ~was~ your brother sad?`;
+
+const isAre = `Anil ~is~ swimming in the pool.
+The school bell ~is~ ringing.
+I ~am~ Anil.
+Ships ~are~ sailing in the sea.
+I ~am~ not a rabbit.
+Birds ~are~ flying in the sky.
+We ~are~ happy.
+Raju and Anil ~are~ friends.
+These children ~are~ playing in the garden.
+The tiger ~is~ feeling hungry.
+I ~am~ not an idiot.
+This ~is~ a lotus flower.`;
+
+let homonyms = `I am not ~allowed~ to drink soda. (aloud)
+My ~aunt~ bought me a new bike! (ant)
+I was so hungry. I ~ate~ my entire dinner. (eight)
+I got a new bat and ~ball~ last week. (bowl)
+What do you want to ~be~  when you grow up? (bee)
+Eat that last green ~bean~ on your plate. (been)
+I ~beat~ you in the race. I was first. (beet)
+I have a teddy ~bear~ at home. (bare)
+I was stung by a ~bee~. (be)
+Where have you ~been~ all this time? (bean)`;
+
+const prepositions = `The key is ~on~ the table. (near, by)
+The fish is ~inside~ the bowl. (outside, on)
+I gave a present ~to~ my mother. (for, by)
+The cat is sitting ~under~ the table. (over, inside)
+Pick the flowers ~from~ the plant. ( to, by)
+Arrange your books ~in~ your book shelf. (on, by)
+The lion lives ~in~ the forest. (on, near)
+We go to school ~by~ bus. (on, to)
+The sun is ~above~ the clouds. (below, over)
+Be careful ~with~ the glasses.(in, on)`
+
+const article = `Picasso was ~an~ artist.
+He is ~an~ honest man.
+I study at ~a~ small university in London.
+We used to live in ~the~ city centre.
+Are you ~a~ teacher?
+It takes me ~an~ hour to get to work.
+My cousin is ~a~ surgeon.
+It looks like it's going to rain. Do you have ~an~ umbrella?
+I have ~a~ one year old daughter.
+Singer Justin comes from ~an~ ordinary family.
+These days Angel enjoys ~the~ life of ~a~ Hollywood film star.
+My brother lives in ~an~ apartment in ~the~ city centre.
+School children in ~the~ UK have to wear ~a~ uniform.
+Is there ~a~ petrol station near here?
+It is very near here. Go straight on and it is on ~the~ left.
+Is France ~a~ European country?
+I've got ~a~ spare ticket for tonight's program. Do you want to come?
+Simon phoned ~the~ police.
+Do you want to go to ~the~ cinema tonight?
+I am reading ~a~ really good book.`;
