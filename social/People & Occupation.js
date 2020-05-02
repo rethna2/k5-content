07bbdeb57f8947bc570fb5c81a275e593e613e46
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-02 15:15:40 +0530

    Create People & Occupation.js

---
+++ social/People & Occupation.js
@@ -0,0 +1,45 @@
+{
+            label: 'People around us - MCQ',
+            type: 'mcq',
+            data: {
+              title: 'Multiple Choice Questions',
+              questions: [
+		              {
+                      qText: `Gives beauty treatment`,
+                      options: `Beautician,Architect,Florist`
+                  },
+		              {
+                      qText: `Mends and repairs shoes`,
+                      options: `Cobbler,Electrician,Butcher`
+                  },
+                  {
+                      qText: `Designs building and houses`,
+                      options: `Architect,Carpenter,Plumber`
+                  },
+                  {
+                      qText: `They write books or novels`,
+                      options: `Author,Newsreader,Teacher`
+                  },
+                  {
+		                  qText: `Make bread and cakes and normally work in a bakery`,
+                      options: `Baker,Cobbler,Waiter`
+                  },
+                  {
+		                  qText: `A person engaged in agriculture`,
+                      options: `Farmer,Lawyer,Painter`
+                  },
+    		          {
+		                  qText: `Servers passengers on an aircraft`,
+                      options: `Air hostess,Farmer,Driver`
+                  },
+		              {
+		                  qText: `Produces videos for the video sharing website Youtube`,
+                      options: `Youtuber,Teacher,Author`
+                  },
+     		          {
+		                  qText: `The person in command of a ship`,
+                      options: `Captain,Driver,Author`
+                  }
+              ] 
+            }
+          }
