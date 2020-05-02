73f7ceaf31898e7d485cea32398f95f63237e447
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-02 15:47:46 +0530

    Update People & Occupation.js

--- social/People & Occupation.js
+++ social/People & Occupation.js
@@ -5,6 +5,110 @@
               title: 'Multiple Choice Questions',
               questions: [
 		  {
+                      qText: `They cut the meat and sell it in their shop`,
+                      options: `Butcher,Barber,Plumber`
+                  },
+		  {
+                      qText: `Makes things from wood including houses and furniture`,
+                      options: `Carpenter,Mechanic,Waiter`
+                  },
+		  {
+                      qText: `Fix problems you have with your teeth`,
+                      options: `Dentist,Actor,Translator`
+                  },   
+		  {
+                      qText: `Repairs machines, especially car motors`,
+                      options: `Mechanic,Carpenter,Painter`
+                  },
+		  {
+                      qText: `Reads the news, normally on television`,
+                      options: `Newsreader,Writer,Author`
+                  },   
+		  {
+                      qText: `A person that takes photos`,
+                      options: `Photographer,Youtuber,Sailor`
+                  },    
+		  {
+                      qText: `Prevent crime`,
+                      options: `Policeman,Fireman,Florist`
+                  },		      
+		  {
+                      qText: `Looks after sick animals`,
+                      options: `Veterinary doctor,Sailor,Captain`
+                  },     
+		   {
+                      qText: `Mows the grass regularly`,
+                      options: `Gardener/Tailor/Waiter`
+                  },    
+		  {
+                      qText: `Delivers mail to your house`,
+                      options: `Postman/Policeman/Driver`
+                  }, 
+		  {
+                      qText: `Works with the accounts of a company`,
+                      options: `Accountant,Cashier,Translator`
+                  }, 
+		  {
+                      qText: `Acts in a play or a movie`,
+                      options: `Actor,Director,Editor`
+                  },     
+		  {
+                      qText: `Passes knowledge to students at school`,
+                      options: `Teacher,Translator,Librarian`
+                  },     
+		  {
+                      qText: `Drives a bus`,
+                      options: `Driver,Captain,Pilot`
+                  },     
+		  {
+                      qText: `Makes clothes for others`,
+                      options: `Tailor,Sailor,Sculptor`
+                  },     
+		  {
+                      qText: `Works for the army`,
+                      options: `Soldier,Policeman,Postman`
+                  }, 
+		  {
+                      qText: `Repairs your water systems or pipes`,
+                      options: `Plumber,Cobbler,Butcher`
+                  },    
+		  {
+                      qText: `Flies a plane`,
+                      options: `Pilot,Driver,Sailor`
+                  },    
+		  {
+                      qText: `Paints pictures or the interior and exterior of buildings`,
+                      options: `Painter,Sculptor,Architect`
+                  },    
+		  {
+                      qText: `Puts out fires`,
+                      options: `Fireman,Policeman,Soldier`
+                  },    
+		  {
+                      qText: `Works with flowers`,
+                      options: `Florist,Baker,Painter`
+                  },    
+		  {
+                      qText: `Translates from one language to another`,
+                      options: `Translator,Writer,Author`
+                  },    
+		  {
+                      qText: `Works in a library`,
+                      options: `Librarian,Teacher,Lecturer`
+                  },    
+		  {
+                      qText: `Gives legal advice`,
+                      options: `Lawyer,Principal,Teacher`
+                  },    
+		  {
+                      qText: `Decides cases in a law court`,
+                      options: `Judge,Captain,Policeman`
+                  },    
+		  {
+                      qText: `Looking after customers and serving food in food outlet`,
+                      options: `Waiter,Baker,Plumber`
+                  },  		      	      
+		  {
                       qText: `Gives beauty treatment`,
                       options: `Beautician,Architect,Florist`
                   },
