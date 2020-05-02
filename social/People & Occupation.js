579f7d39ed9a014dfa3cb20ed2b856768b7ccaa0
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-02 15:17:17 +0530

    Update People & Occupation.js

--- social/People & Occupation.js
+++ social/People & Occupation.js
@@ -4,11 +4,11 @@
             data: {
               title: 'Multiple Choice Questions',
               questions: [
-		              {
+		  {
                       qText: `Gives beauty treatment`,
                       options: `Beautician,Architect,Florist`
                   },
-		              {
+		  {
                       qText: `Mends and repairs shoes`,
                       options: `Cobbler,Electrician,Butcher`
                   },
@@ -21,23 +21,23 @@
                       options: `Author,Newsreader,Teacher`
                   },
                   {
-		                  qText: `Make bread and cakes and normally work in a bakery`,
+		      qText: `Make bread and cakes and normally work in a bakery`,
                       options: `Baker,Cobbler,Waiter`
                   },
                   {
-		                  qText: `A person engaged in agriculture`,
+		      qText: `A person engaged in agriculture`,
                       options: `Farmer,Lawyer,Painter`
                   },
-    		          {
-		                  qText: `Servers passengers on an aircraft`,
+    		  {
+		      qText: `Servers passengers on an aircraft`,
                       options: `Air hostess,Farmer,Driver`
                   },
-		              {
-		                  qText: `Produces videos for the video sharing website Youtube`,
+		  {
+		      qText: `Produces videos for the video sharing website Youtube`,
                       options: `Youtuber,Teacher,Author`
                   },
-     		          {
-		                  qText: `The person in command of a ship`,
+     		  {
+		      qText: `The person in command of a ship`,
                       options: `Captain,Driver,Author`
                   }
               ] 
