3f826ae4b17c9ce9dc6f4a83f88d5237b477b7fe
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-03 18:58:20 +0530

    Create Human organ system.js

---
+++ science/Human organ system.js
@@ -0,0 +1,91 @@
+{
+      label: "Human Organ System",
+      type: "matchByDragDrop",
+      data: {
+        isPractice: true,
+        title: "Drag and drop to match Organ system and its parts.",
+        styles: {
+          fontSize: "1rem",
+          leftSize: 150,
+          rightSize: 150,
+        },
+        list: [
+          ["Respiratory →", "lungs", "trachea", "nose", "larynx"],
+          ["Digestive    →", "stomach", "salivary gland", "small intestine", "mouth", "large intestine", "rectum"],
+          ["Circulatory  →", "heart", "arteries", "veins", "blood"],
+          ["Nervous      →", "brain", "spinal cord", "nerves"],
+          ["Excretory    →", "kidney", "urinary bladder", "ureter"],       
+        ],
+      }
+
+
+{
+            label: 'Human Organ system - MCQ',
+            type: 'mcq',
+            data: {
+              title: 'Multiple Choice Questions',
+              questions: [
+		              {
+                      qText: `Conversion of complex food molecules into simpler molecules `,
+                      options: `digestion, respiration, excretion`
+                  },
+                  {
+                      qText: `Provides oxygen and removes carbon dioxide from the tissues `,
+                      options: `respiratory system, digestive system, nervous system`
+                  },
+                  {
+                      qText: `Transport oxygen and nutrients to all parts of the body `,
+                      options: `circulatory system, immune system, nervous system`
+                  },
+                  {
+                      qText: `Control and coordinate the working of the other systems `,
+                      options: `nervous system, digestive system, respiratory system`
+                  },
+                  {
+                      qText: `Removes the waste products from the body `,
+                      options: `excretory system, nervous system, digestive system`
+                  },
+                  {
+                      qText: `Coiled muscular tube extending from the mouth to the anus `,
+                      options: `alimentary canal, wind pipe, large intestine`
+                  },
+                  {
+                      qText: `They are paired and conical shaped organ `,
+                      options: `lungs, kidney, brain`
+                  },
+                  {
+                      qText: `The process of inhaling and exhaling `,
+                      options: `breathing, eating, sneezing`
+                  },
+                  {
+                      qText: `The last stage of the digestive system `,
+                      options: `large intestine, small intestine, anus`
+                  },
+                  {
+                      qText: `Where does digestion begins in our body?`,
+                      options: `mouth, stomach, food pipe`
+                  },
+                  {
+                      qText: `Carries oxygen around the body `,
+                      options: `red blood cells, white blood cells, nephrons`
+                  },
+                  {
+                      qText: `Attacks the germs that make you sick `,
+                      options: `white blood cells, veins, neurons`
+                  },{
+                      qText: `This forms the structure of your body `,
+                      options: `bones, organs, systems`
+                  },
+                  {
+                      qText: `Only part of the skull that can move `,
+                      options: `mandible, spinal cord, femur`
+                  },
+		  
+               
+		 
+		 
+		 
+		    
+              ] 
+            }
+          }
