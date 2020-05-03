68d0eb3bf96e68096ac3b69d9b1f7c2583ecb113
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-03 20:18:35 +0530

    Update Human organ system.js

--- science/Human organ system.js
+++ science/Human organ system.js
@@ -20,8 +20,8 @@
 
 
 {
-            label: 'Human Organ system - MCQ',
-            type: 'mcq',
+            label: "Human Organ system - MCQ",
+            type: "mcq",
             data: {
               title: 'Multiple Choice Questions',
               questions: [
@@ -72,7 +72,8 @@
                   {
                       qText: `Attacks the germs that make you sick `,
                       options: `white blood cells, veins, neurons`
-                  },{
+                  },
+		  {
                       qText: `This forms the structure of your body `,
                       options: `bones, organs, systems`
                   },
@@ -80,12 +81,82 @@
                       qText: `Only part of the skull that can move `,
                       options: `mandible, spinal cord, femur`
                   },
-		  
-               
-		 
-		 
-		 
-		    
+		  {
+                      qText: `Undigested part of the food move into `,
+                      options: `large intestine, small intestine, liver`
+                  },
+                  {
+                      qText: `The waste material leaves your body through `,
+                      options: `anus, rectum, kidney`
+                  },
+		  {
+                      qText: `Bean shaped organs that clean your blood `,
+                      options: `kidney, brain, lungs`
+                  },
+		  {
+                      qText: `Functional unit of the kidney `,
+                      options: `nephron, neuron, vein`
+                  },
+		  {
+                      qText: `Connects the brain to the spinal cord `,
+                      options: `medulla oblongata, cerebrum, cerebellum`
+                  },
+		  {
+                      qText: `This is required for the better functioning of the brain `,
+                      options: `oxygen, carbon dioxide, nitrogen`
+                  },
+		  {
+                      qText: `Passage through which air passes into the lungs`,
+                      options: `wind pipe, food pipe, alimentary canal`
+                  },
+		  {
+                      qText: `Passage for the entry of air into the windpipe`,
+                      options: `pharynx, esophagus, alimentary canal`
+                  },
+		  {
+                      qText: `Organ placed inside the rib cage`,
+                      options: `heart, brain, kidney`
+                  }, 
+		  {
+                      qText: `Organ placed inside the skull`,
+                      options: `brain, lungs, heart`
+                  }, 
+		  {
+                      qText: `Primary organ of the respiratory system`,
+                      options: `lungs, heart, kidney`
+                  }, 
+		  {
+                      qText: `Human blood is red due to the presence of `,
+                      options: `haemoglobin, white blood cells, plasma`
+                  }, 
+		  {
+                      qText: `Muscular partition between the thoracic and abdominal cavity`,
+                      options: `diaphragm, stomach, intestine`
+                  },     
+		  {
+                      qText: `Group of similar cells work together on a specific task`,
+                      options: `tissue, organ, muscle`
+                  },   
+		  {
+                      qText: `Spinal cord is a part of`,
+                      options: `nervous system, respiratory system, digestive system`
+                  },  
+		  {
+                      qText: `Only part of the body which cannot heal themselves`,
+                      options: `teeth, tongue, eye`
+                  },      
+		  {
+                      qText: `Only part of the body that has no blood supply`,
+                      options: `cornea, teeth, nose`
+                  },     
+		  {
+                      qText: `Group of organs that perform a special function`,
+                      options: `organ system, muscle, tissue`
+                  },      
+		  {
+                      qText: `Muscles which are under our control`,
+                      options: `ovoluntary, involuntary, tissue`
+                  }  
               ] 
             }
           }
