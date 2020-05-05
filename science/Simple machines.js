b36971af8f4002e21bb2063d809b35c1e50af52d
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-05 12:02:33 +0530

    Create Simple machines.js

---
+++ science/Simple machines.js
@@ -0,0 +1,64 @@
+{
+        label: "Simple machines",
+        type: 'passage',
+        list: [
+          {
+            label: 'Simple Machines - Reading',
+            type: 'passage',
+            data: {
+              title: `Simple Machines`,
+              text: ` Simple machines are tools that make work easier. They allow their user to accomplish more work while expending less energy.
+		      There are six different types of simple machines: pulley, lever, wedge, wheel and axle, inclined plane, screw. 
+		      Each simple machine has a special way to make work easier for humans.`         
+		}
+          }, {
+            label: 'Simple Machines - MCQ',
+            type: 'mcq',
+            data: {
+              title: 'Multiple Choice Questions',
+              questions: [
+                  {
+                      qText: `_________ can be used to cut materials apart`,
+                      options: `wedge, screw, inclined plane`
+                  },
+                  {
+                      qText: `The _________ helps you to move the load or change direction of the force.`,
+                      options: `pulley, wedge, wheel and axle`
+                  },
+                  {
+                      qText: `A _________is an object with a slanted edge which is sharp`,
+                      options: `wedge, lever, pulley`
+                  },
+                  {
+                      qText: `_________ allows for heavy objects to slide up to a higher point rather than be lifted.`,
+                      options: `inclined plane, lever, pulley`
+                  },
+                  {
+                      qText: `An old-fashioned well uses a _________  to pull water from underground.`,
+                      options: `pulley, lever, wheel and axle`
+                  },
+                  {
+                      qText: `A rod balanced on a fixed point that can help lift a heavy weight with less effort.`,
+                      options: `lever, wedge, screw `
+                  }
+              ] 
+            }
+          },{
+            label: 'Simple and Compound machines - Classify',
+            type: 'group',
+            data: {
+              title: "Drag and drop: Classify Simple and Compound machines.",
+              types: [
+                {
+                  name: "Simple Machine",
+                  text: `inclined plane, wedge, pulley, lever, screw, wheel and axle `,
+                },
+                {
+                  name: "Compound machine",
+                  text: `crane, clippers, bulldozer, wheelbarrow, pencil sharpener, escalator`,
+                },
+              ],
+            }
+          }
+        ]
+      }
