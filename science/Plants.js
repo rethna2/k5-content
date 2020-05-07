3696749f12a5ddbc4a77e2a8655e80b65e3b1824
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-07 12:17:23 +0530

    Create Plants.js

---
+++ science/Plants.js
@@ -0,0 +1,18 @@
+ {
+            label: 'Plants - MCQ',
+            type: 'mcq',
+            data: {
+              title: 'Multiple Choice Questions',
+              questions: [
+                  {
+                      qText: `Plants give off _________ to help us breathe`,
+                      options: `oxygen, carbon dioxide, nitrogen`
+                  },
+                  {
+                      qText: `The _________ of a plant shows how it grows, lives and dies.`,
+                      options: `life cycle, history, path`
+                  }
+                  
+		          ] 
+            }
+          }
