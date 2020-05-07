1666b9afc65690c2b280d7c0dc1554b1aea2b312
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-07 15:32:41 +0530

    Update Plants.js

--- science/Plants.js
+++ science/Plants.js
@@ -8,6 +8,12 @@
                       qText: `Plants give off _________ to help us breathe`,
                       options: `oxygen, carbon dioxide, nitrogen`
                   },
+		      
+		  **************************************************************************
+		  {
+                      qText: `Plants give off _________ to help us breathe`,
+                      options: `oxygen, carbon dioxide, nitrogen`
+                  },
                   {
                       qText: `The _________ of a plant shows how it grows, lives and dies.`,
                       options: `life cycle, history, path`
