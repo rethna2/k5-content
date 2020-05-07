4652be30774c2cd320d310a751ed6bc3afc3ff0e
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-07 15:26:36 +0530

    Update Plants.js

--- science/Plants.js
+++ science/Plants.js
@@ -11,6 +11,18 @@
                   {
                       qText: `The _________ of a plant shows how it grows, lives and dies.`,
                       options: `life cycle, history, path`
+                  },
+		  {
+                      qText: `Plants need   _________, sunlight and carbon dioxide to live.`,
+                      options: `water, oxygen, air`
+                  },
+		   {
+                      qText: `Plants are green due to the presence of _________.`,
+                      options: `chlorophyll, photosynthesis, sunlight`
+                  },
+		  {
+                      qText: `_________ is the process used by most plants to make their food from sunlight`,
+                      options: `photosynthesis, absorption, germination`
                   }
                   
 		          ] 
