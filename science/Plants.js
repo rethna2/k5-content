854c62b2c7965384b985d5a3c812320cb6d6d0cd
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-07 15:56:11 +0530

    Update Plants.js

--- science/Plants.js
+++ science/Plants.js
@@ -4,10 +4,22 @@
             data: {
               title: 'Multiple Choice Questions',
               questions: [
-                  {
-                      qText: `Plants give off _________ to help us breathe`,
-                      options: `oxygen, carbon dioxide, nitrogen`
-                  }
+                  {   qText: `A tree has a thick and woody stem called _________ `,
+                      options: `trunk, branch, root`
+                  },
+		  {   qText: `_________ are plants which have weak stems and cannot stand straight on their own.`,
+                      options: `creepers, shrubs, herbs`
+                  },
+		  {   qText: `________ are plants that grows upwards with the help of a support `,
+                      options: `climbers, herbs, shrubs`
+                  },
+		  {   qText: `_________ are smaller than trees and have a bushy look.`,
+                      options: `shrubs, herbs, climbers`
+                  },    
+		  {   qText: `Mint, tulsi are examples of _________ `,
+                      options: `herbs, shrubs, creepers`
+                  },   
+		      
 		      
 		  **************************************************************************
 		  {
@@ -29,8 +41,16 @@
 		  {
                       qText: `_________ is the process used by most plants to make their food from sunlight`,
                       options: `photosynthesis, absorption, germination`
-                  }
-                  
-		          ] 
+                  },
+		  {
+                      qText: `The part of the plant that absorbs sunlight is the _________ `,
+                      options: `leaf, flower, root`
+                  },
+		  {
+                      qText: `The _________ of the plant carries water and nutrients to its different parts.`,
+                      options: `stem, leaf, root`
+                  }    
+		  ******************************************************************************************               
+	     ] 
             }
           }
