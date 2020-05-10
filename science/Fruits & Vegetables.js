2c73ee1f0cbd102d56a9790da8b840077fc57517
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-10 11:36:26 +0530

    Create Fruits & Vegetables.js

---
+++ science/Fruits & Vegetables.js
@@ -0,0 +1,18 @@
+{
+            label: 'Fruits & Vegetables - MCQ',
+            type: 'mcq',
+            data: {
+              title: 'Multiple Choice Questions',
+              questions: [
+                  {
+                      qText: `Fruits with rough skin`,
+                      options: `durian, pear, grapes `
+                  },
+                  {
+                      qText: `Fruits with smooth skin`,
+                      options: `pear, pineapple, durian`
+                  },
+              ] 
+              ] 
+            }
+          }
