8dc9abf5feb016320593f42bca0c862e7b516165
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-11 13:09:41 +0530

    Create Animals.js

---
+++ science/Animals.js
@@ -0,0 +1,49 @@
+{
+      label: "Animals",
+      type: "matchByDragDrop",
+      data: {
+        isPractice: true,
+        title: "Drag and drop to match Animals and their sounds.",
+        styles: {
+          fontSize: "1rem",
+          leftSize: 150,
+          rightSize: 150,
+        },
+        list: [
+          ["cat →", "mews"],
+          ["cow →", "moos"], 
+          ["monkey →", "chatters"],
+          ["dog →", "barks"],
+          ["lion →", "roars"],
+          ["goat →", "bleats"],
+          ["horse →", "neighs"],
+          ["elephant →", "trumpets"],
+          ["wolf →", "howls"],
+          ["donkey →", "brays"],
+        ],
+      }
+{
+      label: "Animals",
+      type: "matchByDragDrop",
+      data: {
+        isPractice: true,
+        title: "Drag and drop to match Animals and their homes.",
+        styles: {
+          fontSize: "1rem",
+          leftSize: 150,
+          rightSize: 150,
+        },
+        list: [
+          ["cow →", "shed"], 
+          ["monkey →", "tree"],
+          ["dog →", "kennel"],
+          ["lion →", "den"],
+          ["horse →", "stable"],
+          ["rabbit →", "burrow"],
+          ["bee →", "hive"],
+          ["hen →", "coop"],
+          ["ant →", "anthill"],
+          ["bird →", "nest"],
+          ["spider →", "web"],
+        ],
+      }
