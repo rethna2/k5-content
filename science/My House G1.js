239ec826322c9f9324a641f5909cba2c7a092dbe
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-13 15:44:42 +0530

    Create My House G1.js

---
+++ science/My House G1.js
@@ -0,0 +1,33 @@
+{
+            label: 'My House - MCQ',
+            type: 'mcq',
+            data: {
+              title: 'Multiple Choice Questions',
+              questions: [
+                  {
+                      qText: `We cook our food in _________ `,
+                      options: `kitchen, dining room, storeroom`
+                  },
+                  {
+                      qText: `We sleep and take rest in _________ `,
+                      options: `bedroom, drawing room, dining room`
+                  },
+                    {
+                      qText: `We eat our meals in _________ `,
+                      options: `dining room, drawing room, kitchen`
+                  },
+                   {
+                      qText: `We welcome our friends and relatives in _________ `,
+                      options: `drawing room, dining room, storeroom`
+                  },
+                   {
+                      qText: `We store our things in _________ `,
+                      options: `storeroom, kitchen, dining room `
+                  },
+                   {
+                      qText: `We study in _________ `,
+                      options: `study room, dining room, drawing room`
+                  },
+              ] 
+            }
+          }
