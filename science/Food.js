68abab596f6db4aca3e01244bfd99324dc6d2957
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-13 16:02:55 +0530

    Create Food.js

---
+++ science/Food.js
@@ -0,0 +1,16 @@
+{
+            label: 'Healthy and Unhealthy food - Classify',
+            type: 'group',
+            data: {
+              title: "Drag and drop: Classify Healthy and Unhealthy foods.",
+              types: [
+                {
+                  name: "Healthy food",
+                  text: `milk, egg, banana, grapes, cabbage, cashew, rice `,
+                },
+                {
+                  name: "Unhealthy food",
+                  text: `burger, pizza, potato chips, french fries, soda`,
+                },
+              ],
+            }
