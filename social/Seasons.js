b861d19624ddac6c5aadee7eeee99e7ee9685524
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-14 14:51:27 +0530

    Create Seasons.js

---
+++ social/Seasons.js
@@ -0,0 +1,37 @@
+{
+            label: 'Seasons - MCQ',
+            type: 'mcq',
+            data: {
+              title: 'Multiple Choice Questions',
+              questions: [
+                  {
+                      qText: `The days are shorter and nights are longer in _________.`,
+                      options: `winter, summer, spring`
+                  },
+                  {
+                      qText: `There is water everywhere during _________ season.`,
+                      options: `rainy, winter, summer`
+                  },
+                  {
+                      qText: `We have _________  main seasons in India.`,
+                      options: `three, two, five`
+                  },
+                  {
+                      qText: `The weather is very hot during _________ season.`,
+                      options: `summer, autumn, spring`
+                  },
+                  {
+                      qText: `In which season is the Sun too bright?`,
+                      options: `summer, winter, spring`
+                  },
+                  {
+                      qText: `The same weather enjoyed for a period of time is called a _________.`,
+                      options: `season, period, temperature `
+                  },
+                  {
+                      qText: `_________ changes at different times of the day and night.`,
+                      options: `weather, season, period`
+                  },
+              ] 
+            }
+          }
