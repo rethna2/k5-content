6d46f8cd95c060e563c3fa5d413b1d147652d243
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-14 16:12:46 +0530

    Create Transport.js

---
+++ social/Transport.js
@@ -0,0 +1,33 @@
+ {
+            label: 'Transport- MCQ',
+            type: 'mcq',
+            data: {
+              title: 'Multiple Choice Questions',
+              questions: [
+                  {
+                      qText: `An _________ helps to take sick people to hospital.`,
+                      options: `ambulance, engine, car `
+                  },
+                  {
+                      qText: `A _________ helps us to put out ﬁres.`,
+                      options: `fire engine, police car, bulldozer`
+                  },
+                  {
+                      qText: ` A _________ moves on a track and have an engine`,
+                      options: `train, car, boat`
+                  },
+                  {
+                      qText: ` A _________  has two wheels and a handle.`,
+                      options: `bicycle, car, auto-rickshaw`
+                  },
+                   {
+                      qText: ` Row, row, row your _________ , gently down the stream.`,
+                      options: `boat, bus, rocket`
+                  },
+                   {
+                      qText: ` _________  is the fastest means of transport to reach far-off places.`,
+                      options: `Aeroplane, Car, Boat`
+                  }
+              ] 
+            }
+          },
