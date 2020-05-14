7d106f3120462173252ea2f52a9c0de5f47a13db
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-14 16:27:05 +0530

    Create Things.js

---
+++ science/Things.js
@@ -0,0 +1,17 @@
+{
+            label: 'Floating and Sinking objects - Classify',
+            type: 'group',
+            data: {
+              title: "Drag and drop: Classify Floating and Sinking objects.",
+              types: [
+                {
+                  name: "Floating objects",
+                  text: `leaf, feather, duck, paper, boat`,
+                },
+                {
+                  name: "Sinking objects",
+                  text: `key, spoon, scissor, rock, screw, glass `,
+                },
+              ],
+            }
+          } 
