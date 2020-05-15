035304e189b5039f0e9263fb1d9cb3048fd51d99
sethurani <63959156+sethurani@users.noreply.github.com>
2020-05-15 15:55:31 +0530

    Create Our Universe.js

---
+++ social/Our Universe.js
@@ -0,0 +1,45 @@
+ {
+            label: 'Our Universe - MCQ',
+            type: 'mcq',
+            data: {
+              title: 'Multiple Choice Questions',
+              questions: [
+                  {
+                      qText: `_________ is made of ice and dust and it orbit round the sun.`,
+                      options: `comet, meteor, planet`
+                  },
+                  {
+                      qText: `_________ is the closest planet to the sun`,
+                      options: `Mercury, Earth, Venus `
+                  },
+                  {
+                      qText: `Which of the following is the closest to the Earth?`,
+                      options: `Mars, Jupiter, Mercury`
+                  },
+                  {
+                      qText: `_________ is the largest planet of the solar system.`,
+                      options: `Jupiter, Saturn, Mars`
+                  },
+                  {
+                      qText: `_________ is the smallest planet of the solar system.`,
+                      options: `Mercury, Neptune, Mars`
+                  },
+                  {
+                      qText: `Which of the only planet of the solar system that has life on it ?`,
+                      options: `Earth, Jupiter, Uranus`
+                  },
+                  {
+                      qText: `A group of stars which seems to form a pattern is called a _________ `,
+                      options: `constellation, nebula, gallaxy`
+                  },
+                  {
+                      qText: `_________ is famous for having rings of small pieces of ice and dust around it.`,
+                      options: `Saturn, Jupiter, Neptune`
+                  },
+                  {
+                      qText: `_________ planet has two moons.`,
+                      options: `Mars, Earth, Mercury `
+                  }
+              ] 
+            }
+          }
